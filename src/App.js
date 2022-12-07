44998c043d30e67545710da58c9266838a175494
Sergey Ivushkin <ivushkinserg@gmail.com>
2022-12-07 16:01:24 +0300

    first kata

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function getCount(str) {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+
+    let newstr = str.split('').forEach((item) => {
+      if (vowels.find((vow) => vow === item)) {
+        count++;
+      }
+    });
+
+    return count;
+  }
+
+  console.log(getCount('sdfefaihoeeoouay'));
+
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
