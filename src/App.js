38bbd2de0540ab61d4b5f7b695ae091c9b23f2e3
Sergey Ivushkin <ivushkinserg@gmail.com>
2022-12-11 20:16:19 +0300

    derDieDas

--- src/App.js
+++ src/App.js
@@ -2,34 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  function hasS(att, def) {
-    let itt = Math.max(att.length, def.length);
-    let sur = 0;
-    for (let i = 0; i < itt; i++) {
-      (att[i] || 0) > (def[i] || 0)
-        ? sur--
-        : (att[i] || 0) < (def[i] || 0)
-        ? sur++
-        : (sur += 0);
-      console.log(sur);
+  function derDieDas(wort) {
+    let vow = wort.match(/[aeiouäöü]/gi);
+    if (vow) {
+      vow = vow.length;
     }
-    if (sur > 0) {
-      return true;
-    } else if (sur < 0) {
-      return false;
+    console.log(vow);
+
+    if (vow > 3) {
+      return `der ${wort}`;
+    } else if (vow < 2) {
+      return `das ${wort}`;
     } else {
-      return (
-        def.reduce((acc, item) => acc + item) >=
-        att.reduce((acc, item) => acc + item)
-      );
+      return `die ${wort}`;
     }
   }
-  console.log(
-    hasS(
-      [31, 100, 6, 63, 59, 60, 14, 83, 41, 92],
-      [31, 66, 90, 62, 57, 71, 47, 65, 87, 16]
-    )
-  );
+  console.log(derDieDas('Kndl'));
 }
 
 export default App;
