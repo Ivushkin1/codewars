bd60916bcdc1723e5b3f87ee5fc454e4d673a05c
Sergey Ivushkin <ivushkinserg@gmail.com>
2022-12-11 19:40:13 +0300

    hasSurvived

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  function numberJoy(n) {
-    let sum = n
-      .toString()
-      .split('')
-      .reduce((acc, item) => +acc + +item);
-    let sum2 = +sum.toString().split('').reverse().join('');
-    if (sum * sum2 === n) {
+  function hasS(att, def) {
+    let itt = Math.max(att.length, def.length);
+    let sur = 0;
+    for (let i = 0; i < itt; i++) {
+      (att[i] || 0) > (def[i] || 0)
+        ? sur--
+        : (att[i] || 0) < (def[i] || 0)
+        ? sur++
+        : (sur += 0);
+      console.log(sur);
+    }
+    if (sur > 0) {
       return true;
-    } else return false;
+    } else if (sur < 0) {
+      return false;
+    } else {
+      return (
+        def.reduce((acc, item) => acc + item) >=
+        att.reduce((acc, item) => acc + item)
+      );
+    }
   }
-
-  console.log(numberJoy(1729));
+  console.log(
+    hasS(
+      [31, 100, 6, 63, 59, 60, 14, 83, 41, 92],
+      [31, 66, 90, 62, 57, 71, 47, 65, 87, 16]
+    )
+  );
 }
 
 export default App;
